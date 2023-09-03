51df964a4a4ef08367c23ef799e512c275558b7c
Thomas Ledoux <thomas_ledoux@epam.com>
2023-09-03 09:51:25 +0200

    Add secure option

--- convert-to-localhost.js
+++ convert-to-localhost.js
@@ -3,7 +3,10 @@
   let [tab] = await chrome.tabs.query(queryOptions);
   const url = new URL(tab.url);
   const port = (await chrome.storage.sync.get('port')).port;
+  const secure = (await chrome.storage.sync.get('secure')).secure;
   chrome.tabs.update({
-    url: `http://localhost:${port ?? '3000'}${url.pathname}`,
+    url: `http${secure ? 's' : ''}://localhost:${port ?? '3000'}${
+      url.pathname
+    }`,
   });
 })();
--- options.js
+++ options.js
@@ -1,18 +1,18 @@
 // Saves options to chrome.storage
 const saveOptions = () => {
   const port = document.getElementById('port').value;
-
-  chrome.storage.sync.set({ port });
+  const secure = document.getElementById('secure').checked;
+  chrome.storage.sync.set({ port, secure });
   window.close();
 };
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
-const restoreOptions = () => {
-  chrome.storage.sync.get('port', items => {
-    console.log(items);
-    document.getElementById('port').value = items.port ?? '3000';
-  });
+const restoreOptions = async () => {
+  const syncItems = await chrome.storage.sync.get(['port', 'secure']);
+  console.log(syncItems);
+  document.getElementById('port').value = syncItems.port ?? '3000';
+  document.getElementById('secure').checked = syncItems.secure;
 };
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
